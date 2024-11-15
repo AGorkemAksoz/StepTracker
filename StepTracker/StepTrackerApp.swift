e8943fdd948c5dfe464020890626c0ec28d56be0
AGorkemAksoz <a.gorkemaksoz@gmail.com>
2024-11-15 16:54:35 +0300

    Init Commit

---
+++ StepTracker/StepTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepTrackerApp.swift
+//  StepTracker
+//
+//  Created by Gorkem on 15.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct StepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
