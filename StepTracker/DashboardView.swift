6e1feddce6b32fc59e21fce9dd2ceff377e85929
Ali Görkem Aksöz <96394804+AGorkemAksoz@users.noreply.github.com>
2024-11-25 16:08:25 +0300

    step bar chart interactivity

--- StepTracker/DashboardView.swift
+++ StepTracker/DashboardView.swift
@@ -24,12 +24,22 @@ struct DashboardView: View {
     @AppStorage("hasSeenPermissinPriminig") private var hasSeenPermissionPrimining: Bool = false
     @State private var isShowingPermissionPrmiingSheet: Bool = false
     @State private var selectedMetric: HealtMetricContext = .steps
+    @State private var rawSelectedDate: Date?
     var isSteps: Bool { selectedMetric == .steps }
     var avgStepCount: Double {
         guard !hkManager.stepData.isEmpty else { return 0}
         let totalSteps = hkManager.stepData.reduce(0) { $0 + $1.value }
         return totalSteps/Double(hkManager.stepData.count)
     }
+    
+    var selectedHealtMetric: HealthMetric? {
+        guard let rawSelectedDate else { return nil }
+        
+        return hkManager.stepData.first {
+            Calendar.current.isDate(rawSelectedDate, inSameDayAs: $0.date)
+        }
+    }
+    
     var body: some View {
         NavigationStack {
             ScrollView{
@@ -65,17 +75,31 @@ struct DashboardView: View {
                         .padding(.bottom, 12)
                         
                         Chart {
+                            
+                            if let selectedHealtMetric {
+                                RuleMark(x: .value("Selected Metrick", selectedHealtMetric.date, unit: .day))
+                                    .foregroundStyle(Color.secondary.opacity(0.3))
+                                    .offset(y: -10)
+                                    .annotation(position: .top,
+                                                spacing: 0,
+                                                overflowResolution: .init(x: .fit(to: .chart), y: .disabled)) { annotataionView }
+                            }
+                            
                             RuleMark(y: .value("Average", avgStepCount))
                                 .foregroundStyle(Color.secondary)
                                 .lineStyle(.init(lineWidth: 1, dash: [5]))
                             
                             ForEach(hkManager.stepData) { steps in
-                                BarMark(x: .value("Date", steps.date, unit: .day),
-                                        y: .value("Steps", steps.value))
+                                BarMark(
+                                    x: .value("Date", steps.date, unit: .day),
+                                    y: .value("Steps", steps.value)
+                                )
                                 .foregroundStyle(Color.pink.gradient)
+                                .opacity(rawSelectedDate == nil || steps.date == selectedHealtMetric?.date ? 1.0 : 0.3 )
                             }
                         }
                         .frame(height: 150)
+                        .chartXSelection(value: $rawSelectedDate.animation(.easeInOut))
                         .chartXAxis {
                             AxisMarks {
                                 AxisValueLabel(format: .dateTime.month(.defaultDigits).day())
@@ -130,6 +154,24 @@ struct DashboardView: View {
             }
         }
         .tint(isSteps ? .pink : .indigo)
+    }
+    
+    var annotataionView: some View {
+        VStack(alignment: .leading) {
+            Text(selectedHealtMetric?.date ?? .now, format: .dateTime.weekday(.abbreviated).month(.abbreviated).day())
+                .font(.footnote.bold())
+                .foregroundStyle(.secondary)
+            
+            Text(selectedHealtMetric?.value ?? 0, format: .number.precision(.fractionLength(0)))
+                .fontWeight(.heavy)
+                .foregroundStyle(.pink)
+        }
+        .padding(12)
+        .background(
+            RoundedRectangle(cornerRadius: 4)
+                .fill(Color(.secondarySystemBackground))
+                .shadow(color: .secondary.opacity(0.3), radius: 2, x: 2, y: 2)
+        )
     }
 }
 
